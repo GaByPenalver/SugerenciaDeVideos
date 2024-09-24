ad9a23120a55931365e5c6480ea648a665e3c641
GaBy Peñalver <Gabypealver14@gmail.com>
2024-09-23 22:56:49 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+class Multimedia {
+  constructor(url) {
+      let _url = url; 
+      
+      this.getUrl = function() {
+          return _url;
+      };
+  }
+
+  setInicio() {
+      return "Este método es para realizar un cambio en la URL del video.";
+  }
+}
+
+class Reproductor extends Multimedia {
+  constructor(url, id) {
+      super(url);
+      this.id = id; 
+  }
+
+  playMultimedia() {
+      multimediaModule.play(this.getUrl(), this.id);
+  }
+
+  setInicio(tiempo) {
+      const urlConTiempo = `${this.getUrl()}?start=${tiempo}`;
+      document.getElementById(this.id).setAttribute('src', urlConTiempo);
+  }
+}
+const multimediaModule = (function() {
+  const setUrl = (url, id) => {
+      const iframe = document.getElementById(id);
+      iframe.setAttribute('src', url);
+  };
+
+  
+  return {
+      play: (url, id) => {
+          setUrl(url, id);
+      }
+  };
+})();
+
+const musica = new Reproductor('https://www.youtube.com/embed/jRuXTUwOfoQ', 'musica');
+const peliculas = new Reproductor('https://www.youtube.com/embed/zV_pdLb476k', 'peliculas');
+const series = new Reproductor('https://www.youtube.com/embed/C2-YwsEq0-0', 'series');
+
+musica.playMultimedia();
+peliculas.playMultimedia();
+series.playMultimedia();
